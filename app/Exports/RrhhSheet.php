46ac844015449771d831a92bf928991818cf80b1
Esteban <tebiccr@gmail.com>
2020-09-03 16:28:30 -0400

    RRHH sheet

--- app/Exports/RrhhSheet.php
+++ app/Exports/RrhhSheet.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromQuery;
@@ -8,11 +9,12 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use App\EHR\HETG\Contract;
+use App\EHR\HETG\Rrhh;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
 class RrhhSheet implements FromCollection, WithTitle, WithHeadings, WithMapping, ShouldAutoSize
 {
-    
+
 
     /**
      * @return Builder
@@ -21,24 +23,33 @@ class RrhhSheet implements FromCollection, WithTitle, WithHeadings, WithMapping,
     public function headings(): array
     {
         return [
-          'Rut',
-          'Nombre',
-          'Ley',
-          'Correlativo Contrato',
-          'Título Profesional',
-          'Hrs Semanales Contratadas',
-          'Sistema de Turno (S/N)',
-          'Observaciones debe Identificar (Liberado de Guardia (LG)/Periodo Asistencial Obligatorio(PAO)/ Permiso Gremial)',
-          'Feriados Legales',
-          'Días Descanso Compensatorio (Ley Urgencia)',
-          'Días de Permisos Administrativos',
-          'Días de Congreso o Capacitación',
-          'Tiempo de Lactancia Semanal (min)',
-          'Tiempo de colación semanal (min)',
-          'Fecha de Inicio de Contrato',
-          'Fecha de Termino de Contrato',
-          'Servicio/Unidad',
-          'Cod_Unidad',
+            'ID_DEIS',
+            'Cod_Estab SIRH',
+            'RUT_TIT',
+            'DV_TIT',
+            'Grupo de Riesgo (SI/NO)',
+            'Ausentismo (SI/NO)',
+            'Motivo (maternales, PSGS, Comisiones de estudio)',
+            'RUT Programable',            
+            'DV',
+            'Nombre',
+            'LEY',
+            'Correlativo Contrato',
+            'Titulo Profesional',
+            'Especialidad SIS',
+            'Hrs Semanales contratadas',
+            'Sistema de Turno (S/N)',
+            'Observaciones debe Identificar (Liberado de Guardia (LG)/Periodo Asistencial Obligatorio(PAO))',
+            'Feriados Legales',
+            'Dias Descanso Compensatorio (Ley Urgencia)',
+            'Dias de Permisos Administrativos',
+            'Dias de Congreso o capacitación',
+            'Tiempo de Lactancia semanal (min)',
+            'Tiempo de colación semanal (min)',
+            'Tiempo de permiso gremial  semanal (min)',
+            'Fecha Inicio contrato (AAAAMMDD)',
+            'Fecha termino contrato (AAAAMMDD)',
+            'MATRIZ',
         ];
     }
 
@@ -48,6 +59,7 @@ class RrhhSheet implements FromCollection, WithTitle, WithHeadings, WithMapping,
     public function collection()
     {
         return Contract::all();
+        //return Rrhh::all();
     }
 
     /**
@@ -61,26 +73,36 @@ class RrhhSheet implements FromCollection, WithTitle, WithHeadings, WithMapping,
 
     public function map($contract): array
     {
-        return [
-            $contract->rut,
-            $contract->rrhh->fathers_family.' '.$contract->rrhh->mothers_family.' '.$contract->rrhh->name,
+        return [            
+            $contract->rrhh->id_deis,
+            $contract->rrhh->cod_estab_sirh,
+            $contract->rrhh->rut,
+            $contract->rrhh->dv,
+            $contract->rrhh->risk_group,
+            $contract->rrhh->missing_condition,
+            $contract->rrhh->missing_reason,
+            $contract->rrhh->rut,
+            $contract->rrhh->dv,
+            $contract->rrhh->fullname,
             $contract->law,
             $contract->contract_id,
             $contract->rrhh->job_title,
+            'falta este atributo',
             $contract->weekly_hours,
             $contract->shift_system,
             $contract->obs,
             $contract->legal_holidays,
             $contract->compensatory_rest,
             $contract->administrative_permit,
-            $contract->training_days,            
+            $contract->training_days,
             $contract->breastfeeding_time,
             $contract->weekly_collation,
+            'falta este atributo',
             $contract->contract_start_date,
             $contract->contract_end_date,
-            $contract->unit,
-            $contract->unit_code,
-            //Date::dateTimeToExcel($invoice->created_at),
+            'falta este atribudo',
+
+            
         ];
     }
 
@@ -92,5 +114,4 @@ class RrhhSheet implements FromCollection, WithTitle, WithHeadings, WithMapping,
             //'F' => 11.67,
         ];
     }
-
 }
