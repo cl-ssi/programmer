65ff604c791ff73261e56cc8fd65394a82627120
sick <sick_iqq@hotmail.com>
2020-09-14 11:18:02 -0300

    usuarios Javiera y Fco.

--- database/seeds/HmUserProfessionsTableSeeder.php
+++ database/seeds/HmUserProfessionsTableSeeder.php
@@ -24,9 +24,16 @@ class HmUserProfessionsTableSeeder extends Seeder
         UserProfession::Create(['user_id' => 1,'profession_id'=>10]);
         UserProfession::Create(['user_id' => 1,'profession_id'=>11]);
 
+        UserProfession::Create(['user_id' => 2,'profession_id'=>1]);
+        UserProfession::Create(['user_id' => 2,'profession_id'=>2]);
+        UserProfession::Create(['user_id' => 2,'profession_id'=>3]);
+        UserProfession::Create(['user_id' => 2,'profession_id'=>4]);
+        UserProfession::Create(['user_id' => 2,'profession_id'=>5]);
+        UserProfession::Create(['user_id' => 2,'profession_id'=>6]);
+        UserProfession::Create(['user_id' => 2,'profession_id'=>7]);
+        UserProfession::Create(['user_id' => 2,'profession_id'=>8]);
+        UserProfession::Create(['user_id' => 2,'profession_id'=>9]);
         UserProfession::Create(['user_id' => 2,'profession_id'=>10]);
         UserProfession::Create(['user_id' => 2,'profession_id'=>11]);
-
-        UserProfession::Create(['user_id' => 4,'profession_id'=>5]);
     }
 }
