4c1a42af56d7bb2181b726921cde91ae04ba9f8b
sick <sick_iqq@hotmail.com>
2020-11-03 09:33:18 -0300

    semana volante

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,18 @@
                     {{-- Bienvenido <b>{{session('name')}}</b> - Estás identificado como <b>{{session('profile')}}</b> --}}
                     Bienvenido <b>{{Auth::user()->name}}</b>
                 </div>
+
+            </div>
+
+            <br />
+            <div class="card">
+                <div class="card-header">Novedades</div>
+
+                <div class="card-body">
+                    <div class="alert alert-warning" role="alert">
+                      <b>03/11/2020 - Semana volante</b>: Al arrastrar, modificar o eliminar un evento en programación teórica, se realiza la misma operación cada 7 semanas.
+                    </div>
+                </div>
             </div>
         </div>
     </div>
