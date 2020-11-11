5542384a7fdd12db1d3803622e01e3e910135122
Esteban <sick_iqq@hotmail.com>
2020-11-11 11:10:50 -0300

    fix titulos reports

--- resources/views/ehr/hetg/management/reports/programed_by_services.blade.php
+++ resources/views/ehr/hetg/management/reports/programed_by_services.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<h3 class="mb-3">Reporte de servicios programadas</h3>
+<h3 class="mb-3">Reporte de Servicios Programados</h3>
 
 <table class="table table-sm table-bordered small text-uppercase">
     <thead>
--- resources/views/ehr/hetg/management/reports/programed_specialties.blade.php
+++ resources/views/ehr/hetg/management/reports/programed_specialties.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<h3 class="mb-3">Reporte de especialidades programadas</h3>
+<h3 class="mb-3">Reporte de Especialidades Programadas</h3>
 
 <table class="table table-sm table-bordered small text-uppercase">
     <thead>
